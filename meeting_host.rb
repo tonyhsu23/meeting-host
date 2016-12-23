88ff8448a8cb800066883fc2ad22dc5fd88d85c1
Tony Hsu <t153426789@gmail.com>
2016-12-23 14:52:58 +0800

    Refactor

--- meeting_host.rb
+++ meeting_host.rb
@@ -1,21 +1,19 @@
 #!/usr/bin/env ruby
 require 'gemoji'
 
+TEAM_LEADER  = 'Roger'
 TEAM_MEMBERS = %w(Eugene Yiwen Leon Howard Tony Kellen)
 
-def filter_input(answer)
+def input_convert(answer)
   case answer
-  when /(yes|y)/i
-    roger_exist = true
-  when /(no|n)/i
-    roger_exist = false
-  else
-    roger_exist = false
+  when /(yes|y)/i then true
+  when /(no|n)/i  then false
+  else;                false
   end
 end
 
-def find_host(roger_exist)
-  TEAM_MEMBERS << 'Roger' if roger_exist
+def find_host(leader_exist)
+  TEAM_MEMBERS << TEAM_LEADER if leader_exist
   TEAM_MEMBERS.sample
 end
 
@@ -23,9 +21,13 @@ def gemoji_installed?
   `gem list gemoji -i`
 end
 
+def find_emoji_of(name)
+  Emoji.find_by_alias(name).raw
+end
+
 if gemoji_installed?
-  print "Is Roger here #{Emoji.find_by_alias('thinking').raw}  [y/n] "
-  puts "Today's host is #{find_host(filter_input(gets.chomp))} #{Emoji.find_by_alias('kissing_heart').raw}"
+  print "Is #{TEAM_LEADER} here #{find_emoji_of('thinking')}  [y/n] "
+  puts "Today's host is #{find_host(input_convert(gets.chomp))} #{find_emoji_of('kissing_heart')}"
 else
   puts 'Please install gemoji first.'
 end
